bc053605e18ba3b0c64b75ad5e876f98ae58863c
Om Thorat <thoratom1104@gmail.com>
2025-04-14 08:09:13 +0530

    fix: handle errors in nameDb creation to skip invalid files

--- .github/actions/buildDB/buildDB.js
+++ .github/actions/buildDB/buildDB.js
@@ -1,32 +1,46 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 const core = require('@actions/core');
 const github = require('@actions/github');
-const dir = path.join(__dirname, '..', '..', '..', 'public', 'directory');
+const dir = path.join(__dirname, "..", "..", "..", "public", "directory");
 const files = fs.readdirSync(dir);
 
 const gh = github.getOctokit(core.getInput('GITHUB_TOKEN', { required: true }));
 const nameDb = [];
 
 const build = async () => {
-    for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        console.log({ i, file });
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    console.log({ i, file });
 
-        if (file.endsWith('.json')) {
-            const data = require(path.join(dir, file));
-            
-            await gh.users.getByUsername({username: data.githubId}).then(prof => {
-                data.avatar_url = prof.data.avatar_url;
-                nameDb.push(data);
-            }).catch(console.log);
-        }
+    if (file.endsWith(".json")) {
+      try {
+        const data = require(path.join(dir, file));
+
+        nameDb.push(data);
+
+        await gh.users
+          .getByUsername({ username: data.githubId })
+          .then((prof) => {
+            data.avatar_url = prof.data.avatar_url;
+          });
+          
+      } catch (e) {
+        console.log(e);
+      }
     }
+  }
 };
 
-build().then(() => {
-    fs.writeFileSync(path.join(__dirname, '..', '..', '..', 'public', 'nameDB.json'), JSON.stringify(nameDb));
-}).catch(e => {
-    core.setFailed(`Failed to compile a DB, \n${e.message} `);
-})
+build()
+  .then(() => {
+    fs.writeFileSync(
+      path.join(__dirname, "..", "..", "..", "public", "nameDB.json"),
+      JSON.stringify(nameDb)
+    );
+  })
+  .catch((e) => {
+    // core.setFailed(`Failed to compile a DB, \n${e.message} `);
+    console.log(e);
+  });
